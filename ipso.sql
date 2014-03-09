29c096a4f2b7e5f3acd3e214bc5a8aff610270a5
Martin A. Brooks <martin@hinterlands.org>
2014-03-09 20:40:52 +0000

    initial import

---
+++ ipso.sql
@@ -0,0 +1,41 @@
+drop table if exists ipblocks CASCADE;
+drop table if exists ipallocations CASCADE;
+
+set role ipso;
+
+create table if not exists ipblocks (
+	blockid serial primary key,
+	ipblock cidr NOT NULL,
+	ipblockfamily int,
+	note varchar
+);
+
+create table if not exists ipallocations (
+	allocid serial primary key,
+	blockid bigint references ipblocks (blockid) NOT NULL,
+	firstip inet NOT NULL,
+	ipcount bigint NOT NULL,
+	note varchar
+);
+
+DROP FUNCTION IF EXISTS update_ip_block_family();
+CREATE FUNCTION update_ip_block_family() RETURNS TRIGGER AS
+$$ 
+BEGIN
+    NEW.ipblockfamily:=family(NEW.ipblock);
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER trigger_ipblock_family BEFORE INSERT OR UPDATE OF ipblock ON ipblocks FOR EACH ROW EXECUTE PROCEDURE update_ip_block_family();
+
+INSERT INTO ipblocks (ipblock,note) VALUES ('192.168.0.0/24','Part of RFC1918');
+INSERT INTO ipblocks (ipblock,note) VALUES ('192.168.1.0/24','Part of RFC1918');
+INSERT INTO ipblocks (ipblock,note) VALUES ('10.0.0.0/8','Also part of RFC1918');
+INSERT INTO ipblocks (ipblock,note) VALUES ('2001:ba8:1f1:f12c::/64','Some IPv6');
+
+INSERT INTO ipallocations (blockid,firstip,ipcount,note) VALUES (1,'192.168.0.123',10,'ACME Widgets allocation');
+INSERT INTO ipallocations (blockid,firstip,ipcount,note) VALUES (1,'192.168.0.10',30,'ACME Traps allocation');
+INSERT INTO ipallocations (blockid,firstip,ipcount,note) VALUES (2,'192.168.1.1',10,'ACME Widgets allocation');
+INSERT INTO ipallocations (blockid,firstip,ipcount,note) VALUES (2,'192.168.1.100',30,'ACME Traps allocation');
+INSERT INTO ipallocations (blockid,firstip,ipcount,note) VALUES (4,'2001:ba8:1f1:f12c::2',100000,'ACME Traps v6 allocation');
---
+++ lib/Ipso.pm
@@ -0,0 +1,130 @@
+use strict;
+use warnings;
+use DBD::Pg;
+use YAML::XS qw(LoadFile);
+use Data::Dumper;
+use Switch;
+
+sub dbconnect{
+	my $dbname='ipso';
+	my $host='localhost';
+	my $port=5432;
+	my $options='';
+	my $username='ipso';
+	my $password='ipso';
+	my $dbh = DBI->connect("dbi:Pg:dbname=$dbname;host=$host;port=$port",$username,$password
+		# {AutoCommit => 0, RaiseError => 1, PrintError => 0}
+	);
+	return $dbh;
+}
+
+sub handle_simple_select{
+	my $dbh=shift;
+	my $sql=shift;
+	my $sth = $dbh->prepare("$sql");
+	if ( !defined $sth ) {
+		die "Cannot prepare statement: $DBI::errstr\n";
+	}
+	$sth->execute;
+	my $rows=$sth->fetchall_arrayref();
+	return $rows;
+}
+
+sub handle_simple_insert{
+	my $dbh=shift;
+	my $sql=shift;
+	my $sth = $dbh->prepare("$sql");
+	if ( !defined $sth ) {
+		die "Cannot prepare statement: $DBI::errstr\n";
+	}
+	$sth->execute;
+}
+
+sub getBlockInfo{
+	my %ipblockinfo=();
+	my $dbh=dbconnect();
+	my $ipblocks=handle_simple_select($dbh,'SELECT blockid,ipblock,ipblockfamily,note FROM ipblocks');
+	foreach my $thisblock (@{$ipblocks}){
+		my $blockid=$thisblock->[0];
+		$ipblockinfo{$blockid}{ipblock}=$thisblock->[1];
+		$ipblockinfo{$blockid}{ipfamily}=$thisblock->[2];
+		my ($block,$mask)=split/\//,$ipblockinfo{$blockid}{ipblock};
+		switch ($ipblockinfo{$blockid}{ipfamily}) {
+			case 4 { $ipblockinfo{$blockid}{maxips}=2**(32-$mask);  }
+			case 6 { $ipblockinfo{$blockid}{maxips}=2**(128-$mask); }
+		}
+		$ipblockinfo{$blockid}{note}=$thisblock->[3];
+	}
+	my $ipallocations=handle_simple_select($dbh,'SELECT ipblocks.blockid,COUNT(ipcount),SUM(ipcount) FROM ipblocks,ipallocations WHERE ipblocks.blockid=ipallocations.blockid GROUP BY ipblocks.blockid ORDER BY ipblocks.blockid');
+	foreach my $thisallocation (@{$ipallocations}){
+		my $blockid=$thisallocation->[0];
+		$ipblockinfo{$blockid}{allocations}=$thisallocation->[1];
+		$ipblockinfo{$blockid}{allocated}=$thisallocation->[2];
+	}
+	$dbh->disconnect;
+	return %ipblockinfo;
+}
+
+sub addIPBlock{
+	my $newblock=shift;
+	my $note=shift;
+	my $dbh=dbconnect();
+	my $overlap=handle_simple_select($dbh,"SELECT ipblock FROM ipblocks WHERE '$newblock' <<= ipblock OR '$newblock' >>= ipblock");
+	my $overlapcount=scalar @{$overlap};
+	my @overlaps=();
+	foreach my $ipblock (@{$overlap}){
+		push @overlaps,$ipblock->[0];
+	}
+	if ($overlapcount>0){
+		print "Error: IP block $newblock overlaps $overlapcount existing IP block";
+		if ($overlapcount>1){
+			print "s";
+		}
+		print ": " . join (", ",@overlaps) . "\n";
+		exit;
+	}
+	handle_simple_insert($dbh,"INSERT INTO ipblocks (ipblock,note) VALUES ('$newblock','$note')");
+	print "Block added.\n";
+}
+
+# The regexes used here were taken from
+# http://blog.markhatton.co.uk/2011/03/15/regular-expressions-for-ip-addresses-cidr-ranges-and-hostnames/
+
+sub is_ipv4_address{
+	my $ip=shift;
+	if ($ip =~ /^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$/){
+		return 1;
+	} else {
+		return 0;
+	}
+}
+
+sub is_ipv4_cidr{
+	my $cidr=shift;
+	if ($cidr =~ /^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])(\/(\d|[1-2]\d|3[0-2]))$/){
+		return 1;
+	} else {
+		return 0;
+	}
+}
+
+sub is_ipv6_address{
+	my $ip=shift;
+	if ($ip =~ /^\s*((([0-9A-Fa-f]{1,4}:){7}([0-9A-Fa-f]{1,4}|:))|(([0-9A-Fa-f]{1,4}:){6}(:[0-9A-Fa-f]{1,4}|((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){5}(((:[0-9A-Fa-f]{1,4}){1,2})|:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){4}(((:[0-9A-Fa-f]{1,4}){1,3})|((:[0-9A-Fa-f]{1,4})?:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){3}(((:[0-9A-Fa-f]{1,4}){1,4})|((:[0-9A-Fa-f]{1,4}){0,2}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){2}(((:[0-9A-Fa-f]{1,4}){1,5})|((:[0-9A-Fa-f]{1,4}){0,3}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){1}(((:[0-9A-Fa-f]{1,4}){1,6})|((:[0-9A-Fa-f]{1,4}){0,4}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(:(((:[0-9A-Fa-f]{1,4}){1,7})|((:[0-9A-Fa-f]{1,4}){0,5}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:)))(%.+)?\s*/){
+		return 1;
+	} else {
+		return 0;
+	}
+}
+	
+sub is_ipv6_cidr{
+	my $cidr=shift;
+	if ($cidr =~ /^\s*((([0-9A-Fa-f]{1,4}:){7}([0-9A-Fa-f]{1,4}|:))|(([0-9A-Fa-f]{1,4}:){6}(:[0-9A-Fa-f]{1,4}|((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){5}(((:[0-9A-Fa-f]{1,4}){1,2})|:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){4}(((:[0-9A-Fa-f]{1,4}){1,3})|((:[0-9A-Fa-f]{1,4})?:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){3}(((:[0-9A-Fa-f]{1,4}){1,4})|((:[0-9A-Fa-f]{1,4}){0,2}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){2}(((:[0-9A-Fa-f]{1,4}){1,5})|((:[0-9A-Fa-f]{1,4}){0,3}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){1}(((:[0-9A-Fa-f]{1,4}){1,6})|((:[0-9A-Fa-f]{1,4}){0,4}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(:(((:[0-9A-Fa-f]{1,4}){1,7})|((:[0-9A-Fa-f]{1,4}){0,5}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:)))(%.+)?\s*(\/(\d|\d\d|1[0-1]\d|12[0-8]))$/){
+		return 1;
+	} else {
+		return 0;
+	}
+}
+
+1;
+
